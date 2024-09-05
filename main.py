70ec0d9fc4d45fbe0e779c852f594bd526336459
Vladlenuser <vladislavlojko40@gmail.com>
2024-09-05 10:28:38 +0300

    fi commit

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram import Router
 from aiogram.filters import Command
-import os
+from aiohttp import web
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-API_TOKEN = '7197491181:AAHAUrAYczXlRBqk_FbgsWctz40MKQyhzWE'  # Замените 'YOUR_TOKEN' на токен вашего бота
+API_TOKEN = '7197491181:AAHAUrAYczXlRBqk_FbgsWctz40MKQyhzWE'
+WEBHOOK_URL = 'https://test-wfip.onrender.com'  # Замените на ваш домен
 PORT = 3000
 
 bot = Bot(token=API_TOKEN)
@@ -24,9 +25,23 @@ async def send_welcome(message: types.Message):
     ])
     await message.answer("Привет! Открываю карту:", reply_markup=keyboard)
 
-async def main():
-    dp.include_router(router)
-    await dp.start_polling(bot)
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+async def handle_webhook(request):
+    json = await request.json()
+    update = types.Update(**json)
+    await dp.feed_update(update)
+    return web.Response()
+
+app = web.Application()
+app.router.add_post('/webhook', handle_webhook)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    dp.include_router(router)
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+    web.run_app(app, port=PORT)
